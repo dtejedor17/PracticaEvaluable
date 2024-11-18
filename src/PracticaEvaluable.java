3bba5c286dffa7f322edc7e31960c48f0760313c
dtejedor17 <diego.tejper@iesjulianmarias.es>
2024-11-18 17:32:25 +0100

    Lunes

--- src/PracticaEvaluable.java
+++ src/PracticaEvaluable.java
@@ -11,42 +11,49 @@ public class PracticaEvaluable {
         int turno;
         turno = (int) (Math.random()*2);
         apuestaMaquina = (int)(Math.random()*canicasMaquina);
-
+        System.out.println("Comienza el juego");
         while (canicasHumano !=0 && canicasMaquina !=0) {
             if (turno == 1) {
-                System.out.println("Comienza el juego");
+
                 System.out.println("El Jugador " + jugadorHumano + " intenta adivinar si el Jugador " + jugadorMaquina + " tiene un número par o impar ");
                 System.out.println();
                 System.out.println("************************* TURNO JUGADOR "+jugadorMaquina+ " **************************");
-                System.out.println("Jugador "+jugadorHumano+", ¿Cuántas canicas quieres apostar?: ");
+                System.out.print("Jugador "+jugadorHumano+", ¿Cuántas canicas quieres apostar?: ");
                 apuestaHumano = sc.nextInt();
+                System.out.println();
                 if (apuestaHumano % 2 == 0 && apuestaMaquina == 1){
                     System.out.println("Jugador 2 cree que el Jugador 1 tiene un número par");
+                    System.out.println();
                     System.out.println("Resultado");
                     System.out.println("Jugador "+jugadorHumano+" GANAS "+apuestaHumano+ " canicas ya que el Jugador "+jugadorMaquina+" tenía "+apuestaMaquina+" canicas.");
                 }else {
                     System.out.println("Jugador 2 cree que el Jugador 1 tiene un número impar");
+                    System.out.println();
                     System.out.println("Resultado");
                     System.out.println("Jugador " + jugadorHumano + " PIERDES " + apuestaHumano + " canicas ya que el Jugador " + jugadorMaquina + " tenía " + apuestaMaquina + " canicas.");
                 }
 
 
             }else{
-                System.out.println("Comienza el juego");
+
                 System.out.println("El Jugador " + jugadorMaquina + " intenta adivinar si el Jugador " + jugadorHumano + " tiene un número par o impar ");
                 System.out.println();
                 System.out.println("************************* TURNO JUGADOR "+jugadorHumano+ " **************************");
-                System.out.println("Jugador "+jugadorHumano+", ¿Cuántas canicas quieres apostar?: ");
+                System.out.print("Jugador "+jugadorHumano+", ¿Cuántas canicas quieres apostar?: ");
                 apuestaHumano = sc.nextInt();
-                System.out.println("Jugador "+jugadorHumano+" ¿Que crees que tiene el Jugador 2? (Par / Impar:)");
+                System.out.println();
+                System.out.print("Jugador "+jugadorHumano+" ¿Que crees que tiene el Jugador 2? (Par / Impar:)");
                 parOimparHumano = sc.next();
                 parOimparHumano = parOimparHumano.toLowerCase();
+                System.out.println();
                 if (parOimparHumano.equals("par")) {
+                    System.out.println();
                     System.out.println("Resultado");
                     System.out.println("Jugador "+jugadorHumano+" GANAS "+apuestaHumano+ " canicas ya que el Jugador "+jugadorMaquina+" tenía "+apuestaMaquina+" canicas.");
 
                 }else if (parOimparHumano.equals("impar")){
                     System.out.println("Jugador 2 cree que el Jugador 1 tiene un número impar");
+                    System.out.println();
                     System.out.println("Resultado");
                     System.out.println("Jugador " + jugadorHumano + " PIERDES " + apuestaHumano + " canicas ya que el Jugador " + jugadorMaquina + " tenía " + apuestaMaquina + " canicas.");
 
