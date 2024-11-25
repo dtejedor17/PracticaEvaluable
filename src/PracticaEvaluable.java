058be9f1daa0151014580e1cc70809c6d8d837f5
dtejedor17 <diego.tejper@iesjulianmarias.es>
2024-11-25 04:47:20 +0100

    Lunes

--- src/PracticaEvaluable.java
+++ src/PracticaEvaluable.java
@@ -1,21 +1,18 @@
 import java.util.InputMismatchException;
+import java.util.Random;
 import java.util.Scanner;
 public class PracticaEvaluable {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int canicasHumano = 10, canicasMaquina= 10;
-        int apuestaHumano =0, apuestaMaquina;
-        int jugadorHumano=1,jugadorMaquina=2;
+        int canicasHumano = 10, canicasMaquina= 10, apuestaHumano =0, apuestaMaquina, jugadorHumano=1,jugadorMaquina=2, turno, resultado;
         String parOimparHumano;
-        int parOimparMaquina, resultado;
-        boolean ganadorJugadorHumano = false;
-        int turno;
+        boolean ganadorJugadorHumano = false, parOimparMaquina;
+        Random rand  = new Random();
         turno = (int) (Math.random()*2);
         System.out.println("Comienza el juego");
         while (canicasHumano !=0 && canicasMaquina !=0) {
             if (turno == 0) {
-
-                System.out.println("El Jugador " + jugadorHumano + " intenta adivinar si el Jugador " + jugadorMaquina + " tiene un número par o impar ");
+                System.out.printf("\nEl Jugador " + jugadorHumano + " intenta adivinar si el Jugador " + jugadorMaquina + " tiene un número par o impar \n");
                 System.out.println();
                 System.out.println("************************* TURNO JUGADOR "+jugadorMaquina+ " **************************");
                 System.out.print("Jugador "+jugadorHumano+", ¿Cuántas canicas quieres apostar?:");
@@ -28,32 +25,31 @@ public class PracticaEvaluable {
                     apuestaHumano = sc.nextInt();
                 }
                 apuestaMaquina = (int)(Math.random()*canicasMaquina)+1;
-                parOimparMaquina = (int)(Math.random()*2);
-                if (parOimparMaquina == 0) {
+                parOimparMaquina = rand.nextBoolean();
+                if (parOimparMaquina) {
                     System.out.println("Jugador 2 cree que el Jugador 1 tiene un número par");
-                    if (apuestaHumano % 2 == 1) {
-                        ganadorJugadorHumano = true;
-                    }
-                }else if (parOimparMaquina == 1) {
+                }else{
                     System.out.println("Jugador 2 cree que el Jugador 1 tiene un número impar");
-                    if (apuestaHumano % 2 == 0) {
-                       ganadorJugadorHumano = true;
-                    }
-                    System.out.printf("\nResultado");
+                }
+                if (parOimparMaquina && apuestaHumano % 2 == 1 || !parOimparMaquina && apuestaHumano % 2 == 0) {
+                    ganadorJugadorHumano = true;
+                }else {
+                    ganadorJugadorHumano = false;
+                }
+                    System.out.printf("\nResultado:\n\n");
                     resultado = (Math.min(apuestaHumano, apuestaMaquina));
-                    if (ganadorJugadorHumano) {
-                        System.out.println("Jugador "+jugadorHumano+" GANAS "+resultado+ " canicas ya que el Jugador "+jugadorMaquina+" tenía "+apuestaMaquina+" canicas.");
-                        canicasHumano = canicasHumano+resultado;
-                        canicasMaquina = canicasMaquina-resultado;
-                        System.out.println("Jugador "+jugadorHumano+ " tiene "+canicasHumano+" canicas.");
-                        System.out.println("Jugador "+jugadorMaquina+ " tiene "+canicasMaquina+" canicas.");
-                    }else{
-                        System.out.println("Jugador "+jugadorHumano+" PIERDES "+resultado+ " canicas ya que el Jugador "+jugadorMaquina+" tenía "+apuestaMaquina+" canicas.");
-                        canicasHumano = canicasHumano-resultado;
-                        canicasMaquina = canicasMaquina+resultado;
-                        System.out.println("Jugador "+jugadorHumano+ " tiene "+canicasHumano+" canicas.");
-                        System.out.println("Jugador "+jugadorMaquina+ " tiene "+canicasMaquina+" canicas.");
-                    }
+                if (ganadorJugadorHumano) {
+                    System.out.println("Jugador "+jugadorHumano+" GANAS "+resultado+ " canicas ya que el Jugador "+jugadorMaquina+" tenía "+apuestaMaquina+" canicas.");
+                    canicasHumano = canicasHumano+resultado;
+                    canicasMaquina = canicasMaquina-resultado;
+                    System.out.println("Jugador "+jugadorHumano+ " tiene "+canicasHumano+" canicas.");
+                    System.out.println("Jugador "+jugadorMaquina+ " tiene "+canicasMaquina+" canicas.");
+                }else{
+                    System.out.println("Jugador "+jugadorHumano+" PIERDES "+resultado+ " canicas ya que el Jugador "+jugadorMaquina+" tenía "+apuestaMaquina+" canicas.");
+                    canicasHumano = canicasHumano-resultado;
+                    canicasMaquina = canicasMaquina+resultado;
+                    System.out.println("Jugador "+jugadorHumano+ " tiene "+canicasHumano+" canicas.");
+                    System.out.println("Jugador "+jugadorMaquina+ " tiene "+canicasMaquina+" canicas.");
                 }
                 turno = 1;
             }else{
@@ -66,21 +62,18 @@ public class PracticaEvaluable {
                     System.err.println("Tienes que introducir un numero entre 1 y "+canicasHumano);
                     apuestaHumano = sc.nextInt();
                 }
-
-                System.out.printf("\nJugador "+jugadorHumano+" ¿Que crees que tiene el Jugador 2? (Par / Impar:)");
+                System.out.printf("\nJugador "+jugadorHumano+" ¿Que crees que tiene el Jugador 2? (Par / Impar):");
                 parOimparHumano = sc.next();
                 parOimparHumano = parOimparHumano.toLowerCase();
-                System.out.println();
-                if (parOimparHumano.equals("par")||parOimparHumano.equals("impar")){
-                    if (canicasMaquina % 2 == 0) {
-                        ganadorJugadorHumano = true;
-
-                    }else if (canicasMaquina % 2 == 1) {
-                        ganadorJugadorHumano = false;
-                    }
-                }
-                System.out.printf("Resultado\n");
                 apuestaMaquina = (int)(Math.random()*canicasMaquina)+1;
+                System.out.println();
+                if (parOimparHumano.equals("par") && apuestaMaquina % 2 == 0 || parOimparHumano.equals("impar") && apuestaMaquina % 2 == 1) {
+                    ganadorJugadorHumano = true;
+                }else{
+                    ganadorJugadorHumano = false;
+                }
+                System.out.printf("Resultado:\n\n");
+
                 resultado = (Math.min(apuestaHumano, apuestaMaquina));
                 if (ganadorJugadorHumano) {
                     System.out.println("Jugador " + jugadorHumano + " GANAS " + resultado + " canicas ya que el Jugador " + jugadorMaquina + " tenía " + apuestaMaquina + " canicas.");
